08337213da7dadeb80d1cfb603958fd5294d5f26
Eron Lloyd <eronlloyd@protonmail.com>
2024-11-26 21:33:01 -0500

    Fixed naming clash

--- democracrm/lobbying/models.py
+++ democracrm/lobbying/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 from core.models import CRMBase, CRMTreeBase
 from organizing.models import Campaign
-from places.models import Boundary
+#from places.models import Boundary
 
 
 class PoliticalParty(CRMBase):
